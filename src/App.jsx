b47f3a80fdc60994e110c8ecaed3ec12edf51be7
Jacob Ismael <jacob.ismael@gmail.com>
2021-06-02 22:19:51 -0700

    added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,40 @@
 import React from 'react';
 import "./App.css";
 
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 import Navigation from "./components/Navbar.jsx"
-import {Input, Button, InputGroup, InputGroupAddon} from 'reactstrap'
+import Toy from "./components/Toy.jsx"
+import { Input, Button, InputGroup, InputGroupAddon } from 'reactstrap'
 function App() {
-  
+
   return (
     <div className="App">
-      <Navigation/>
-      <div className="Banner">
-        <div className="title1">
-          <h1 className="title2">
-            Looking for a Toy Product Review?
-          </h1>
-         {/* <Input className="searchbar" placeholder="Find a Toy"></Input> */}
-         <InputGroup>
-          <Input className="searchbar" placeholder="Find a Toy"/>
-          <InputGroupAddon addonType="prepend"><Button>Search</Button></InputGroupAddon>
-        </InputGroup>
-        </div> 
-        </div>
+
+
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <Navigation />
+            <div className="Banner">
+              <div className="title1">
+                <h1 className="title2">Looking for a Toy Product Review?</h1>
+                <InputGroup>
+                  <Input className="searchbar" placeholder="Find a Toy" />
+                  <InputGroupAddon addonType="prepend"><Button>Search</Button></InputGroupAddon>
+                </InputGroup>
+              </div>
+            </div>
+            </Route>
+          <Route path="/toy/:name" component={Toy} />
+        </Switch>
+      </Router>
+
     </div>
   );
 }
