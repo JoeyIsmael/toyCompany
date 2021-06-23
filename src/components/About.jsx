2c1ef3450c224c0e771d79b3f50208b0e0a8dae6
Joseph Ismael <joseph.a.ismael@gmail.com>
2021-06-23 16:35:58 -0700

    aboutstuff

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,6 +6,13 @@ function About() {
         <div className="App">
             <section class="contact section" id="contact">
                 <h2 class="section-title">About Us</h2>
+                <p class="aboutText">We love toys! Kids Rate Toys is a 100% kid-run business that reviews kids' toys from a kid's perspective. 
+                    We are different from other companies that review or advertise toys because kids know toys better than any adult. 
+                    Kids Rate Toys started as a group of friends - our parents and many of us used Amazon and other businesses that sold toys. One of our parents even sold toys on Amazon! 
+                    However, many of the reviews were by adults and were not the true thoughts kids have about toys. 
+                    Thus, we founded Kids Rate Toys to give honest reviews of toys from a kid's perspective. 
+                    On our website kidsratetoys.com, we post daily videos and reviews of toys!
+                </p>
             </section>
         </div>
     );
