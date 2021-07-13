db55ecf9a1e03093d300dcdf5d868c042dee0556
Jacob Ismael <jacob.ismael@gmail.com>
2021-07-12 18:08:42 -0700

    added logging out

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,32 +4,76 @@ import {
 } from "react-router-dom";
 import '../App.css'
 
-const Navigation = (props) => {
+import firebase from "../firebase.js";
 
-  return (
-    <div>
-      <header className="l-header">
-        <nav className="nav bd-grid">
-          <div>
-          <Link to="/" className="nav__logo">Kids Rate Toys</Link>
-          </div>
+class Navigation extends React.Component {
 
-          <div className="nav__menu" id="nav-menu">
-            <ul className="nav__list">
-              <li className="nav__item"><Link to="/" className="nav__link active">Home</Link></li>
-              <li className="nav__item"><Link to="/about" className="nav__link">About</Link></li>
-              <li className="nav__item"><Link to="/contact" className="nav__link">Contact</Link></li>
-              <li className="nav__item"><Link to="/signup" className="nav__link">Sign Up</Link></li>
-            </ul>
-          </div>
 
-          <div className="nav__toggle" id="nav-toggle">
-            <i className='bx bx-menu'></i>
-          </div>
-        </nav>
-      </header>
-    </div>
-  );
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      logged: false,
+    }
+  }
+
+  componentDidMount() {
+
+
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        // User is signed in.
+        this.setState({ logged: true })
+      } else {
+        // No user is signed in.
+      }
+    });
+  }
+
+  signOut() {
+    try {
+      firebase.auth().signOut()
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  render() {
+
+    let login = <li className="nav__item"><Link to="/signup" className="nav__link">Sign Up</Link></li>
+
+    if (this.state.logged) {
+      login = (
+        <li className="nav__item" onClick={this.signOut}><Link className="nav__link">Sign Out</Link></li>
+      )
+    }
+
+    return (
+      <div>
+        <header className="l-header">
+          <nav className="nav bd-grid">
+            <div>
+              <Link to="/" className="nav__logo">Kids Rate Toys</Link>
+            </div>
+
+            <div className="nav__menu" id="nav-menu">
+              <ul className="nav__list">
+                <li className="nav__item"><Link to="/" className="nav__link active">Home</Link></li>
+                <li className="nav__item"><Link to="/about" className="nav__link">About</Link></li>
+                <li className="nav__item"><Link to="/contact" className="nav__link">Contact</Link></li>
+                {login}
+              </ul>
+            </div>
+
+            <div className="nav__toggle" id="nav-toggle">
+              <i className='bx bx-menu'></i>
+            </div>
+          </nav>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default Navigation;
